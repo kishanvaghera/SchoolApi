22d1f9d48c51d7be2fa92b99f746cf0298db8e79
AkashMakwana <AkashMakwana>
2022-06-11 09:04:18 +0530

    Attendance Api Changes

--- dailyAttendance.php
+++ dailyAttendance.php
@@ -16,11 +16,24 @@ if($_POST['action'] == "AddAttendance"){
     $iClassId = $_POST['iClassId'];
     $iSectionId = $_POST['iSectionId'];
 
-    $selStudent = $mfp->mf_query("SELECT * FROM users WHERE iRoleId = 5 AND eStatus = 'y' AND iClassId = '".$iClassId."' AND iSectionId = '".$iSectionId."' AND iSchoolId = '1'");
+    $rtnArr = array();
+    $returnArr = array();
+    $selStudent = $mfp->mf_query("SELECT iUserEmpId,vFullName FROM users WHERE iRoleId = 5 AND eStatus = 'y' AND iClassId = '".$iClassId."' AND iSectionId = '".$iSectionId."' AND iSchoolId = '1'");
     if($mfp->mf_affected_rows()>0){
         while($rowStudent = $mfp->mf_fetch_array($selStudent)){
-            
+            $returnArr[] = $rowStudent;
         }
+        $rtnArr = array("status"=>200,"data"=>$returnArr);
+    }else{
+        $rtnArr = array("status"=>412,"data"=>"No Data Found!");
     }
+
+    echo json_encode($rtnArr);
+    exit;
+}else if($_POST['action'] == "GetDailyAttendace"){
+    $iClassId = $_POST['iClassId'];
+    $iSectionId = $_POST['iSectionId'];
+    $iMonth = $_POST['iMonth'];
+    $iYear = $_POST['iYear'];
 }
 ?>
