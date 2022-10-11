e4dd37fb1d3a6415f34314da0641358fb5a43dba
AkashMakwana <AkashMakwana>
2022-10-11 22:20:28 +0530

    ---

--- NodeApi/index.js
+++ NodeApi/index.js
@@ -1,13 +1,29 @@
 const express = require("express");
-const app=express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const app = express();
 
+// adding Helmet to enhance your API's security
+app.use(helmet());
+
+// using bodyParser to parse JSON bodies into JS objects
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
-const AuthRouter=require("./Auth");
+// enabling CORS for all requests
+app.use(cors());
 
-app.use("/loginApi",AuthRouter);
+// adding morgan to log HTTP requests
+// app.use(morgan('combined'));
+// app.use(express.urlencoded({ extended: true }));
 
-app.listen(8000,()=>{
+const AuthRouter = require("./Auth");
+const MasterRouter = require("./masterAction");
+
+app.use("/loginApi", AuthRouter);
+app.use("/masterActionApi", MasterRouter);
+
+app.listen(8000, () => {
     console.log("server is responding.");
 })
