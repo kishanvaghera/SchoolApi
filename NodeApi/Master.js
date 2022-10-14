7b9f714eeef47bfc8267b2c8f8596af99a547cc2
kishanvaghera <kishanvaghera8219@gmail.com>
2022-10-15 01:28:28 +0530

    new api

--- NodeApi/Master.js
+++ NodeApi/Master.js
@@ -3,6 +3,7 @@ const router=express.Router();
 const con=require("./config");
 const sha1 = require('sha1');
 const {InsertFunc} =require("./CommoFun");
+var moment = require('moment'); // require
 
 router.post("/",(req,res)=>{
     const data=req.body;
@@ -24,6 +25,18 @@ router.post("/",(req,res)=>{
                 res.send({status:412,message:"No Record Found!"});
             }else{
                 res.send({status:200,message:"Subject has been added Succesfully."});
+            }
+        });
+    }else if(data.action=="addEvents"){
+        data.data['dFromDate']=moment(data.data['dFromDate']).format('YYYY-MM-DD');
+        data.data['dToDate']=moment(data.data['dToDate']).format('YYYY-MM-DD');
+        const insData=InsertFunc(data.data);
+        var insValue = insData.keyValues.map(d => `'${d}'`).join(','); 
+        con.query("INSERT INTO events ("+insData.keyNames.toString()+") VALUES ("+insValue+")", function (err, result) {  
+            if(err){
+                res.send({status:412,message:"No Record Found!"});
+            }else{
+                res.send({status:200,message:"Event has been added Succesfully."});
             }
         });
     }
--- NodeApi/Student.js
+++ NodeApi/Student.js
@@ -3,6 +3,8 @@ const router=express.Router();
 const con=require("./config");
 const sha1 = require('sha1');
 const {InsertFunc} =require("./CommoFun");
+var moment = require('moment'); // require
+
 //Login API
 router.post("/",(req,res)=>{
     const data=req.body;
@@ -46,6 +48,18 @@ router.post("/",(req,res)=>{
                 res.send({status:412,message:"No Record Found!"});
             }else{
                 res.send({status:200,message:"Syllabus has been added Succesfully."});
+            }
+        });
+    }else if(data.action=="addExamTimeTable"){
+        data.data['dStartDate']=moment(data.data['dStartDate']).format('YYYY-MM-DD');
+        data.data['dEndDate']=moment(data.data['dEndDate']).format('YYYY-MM-DD');
+        const insData=InsertFunc(data.data);
+        var insValue = insData.keyValues.map(d => `'${d}'`).join(','); 
+        con.query("INSERT INTO exams ("+insData.keyNames.toString()+") VALUES ("+insValue+")", function (err, result) {  
+            if(err){
+                res.send({status:412,message:"No Record Found!"});
+            }else{
+                res.send({status:200,message:"Exam Time Table has been added Succesfully."});
             }
         });
     }else{
